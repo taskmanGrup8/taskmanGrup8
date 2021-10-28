1da3a8bb71c9b440ffa8bc7537bb662a100c8273
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-10-28 22:46:12 +0200

    Add files via upload
    Classes per la paginació

---
+++ src/main/java/com/grup8/taskman/app/util/PageItem.java
@@ -0,0 +1,23 @@
+package com.grup8.taskman.app.util;
+
+public class PageItem {
+	
+	private int numero;
+	private boolean actual;
+	public PageItem(int numero, boolean actual) {
+		
+		this.numero = numero;
+		this.actual = actual;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public boolean isActual() {
+		return actual;
+	}
+	
+	
+	
+	
+
+}
