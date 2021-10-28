14d60871b4eacf7025e548bacd703007a4391c41
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-10-28 22:48:25 +0200

    Add files via upload
    Actualització amb més dades

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,32 @@
 INSERT INTO rols (codigo, nombre) VALUES('ROLE_ADMIN', 'Administrador');
 INSERT INTO rols (codigo, nombre) VALUES('ROLE_USER', 'Usuario');
 
+
+INSERT INTO departaments (codigo, nombre) VALUES('INF', 'Informàtica');
+
+
+
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Esteban Gutiérrez', '12345678A', 'Sergio', '1111', true, '666554411', 1, 'sergio@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Gómez Casas', '12345679A', 'Pablo', '2222', true, '666554422', 2, 'pablo@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'López Carrasco', '22345678A', 'Aitor', '1111', true, '666554411', 1, 'aitor@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'García Iniesta', '32345679A', 'Luz', '2222', true, '666554422', 2, 'luz@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Salas Escudero', '42345678A', 'Isabel', '1111', true, '666554411', 1, 'isabel@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Carvajal Ramos', '52345679A', 'Sara', '2222', true, '666554422', 2, 'sara@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Fernández Valverde', '62345678A', 'Luis', '1111', true, '666554411', 1, 'luis@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Vázquez Asensio', '72345679A', 'Pablo', '2222', true, '666554422', 2, 'lucas@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Alarcón Blanco', '82345678A', 'Miguel', '1111', true, '666554411', 1, 'miguel@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Hermoso Corredera', '92345679A', 'Marta', '2222', true, '666554422', 2, 'marta@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Piqué Alba', '13345678A', 'Jennifer', '1111', true, '666554411', 1, 'jenny@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Araujo Mingueza', '14345679A', 'Ana', '2222', true, '666554422', 2, 'ana@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Busquets Soler', '15345678A', 'Carlos', '1111', true, '666554411', 1, 'carlos@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Merino Torres', '16345679A', 'Ferran', '2222', true, '666554422', 2, 'ferran@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Hernández Soldevilla', '17345678A', 'Robert', '1111', true, '666554411', 1, 'roberto@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Luque Santos', '18345679A', 'Ariadna', '2222', true, '666554422', 2, 'ari@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Ortega Vílchez', '19345678A', 'Ester', '1111', true, '666554411', 1, 'ester@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'León Moreno', '12445679A', 'Mónica', '2222', true, '666554422', 2, 'monica@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Martínez Llorente', '12545678A', 'Adrián', '1111', true, '666554411', 1, 'adri@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Ufarte Vidal', '12545679A', 'Nacho', '2222', true, '666554422', 2, 'nacho@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Montoya Ruiz', '12645678A', 'Víctor', '1111', true, '666554411', 1, 'victor@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Ferrer Bakero', '11111111A', 'Maria', '1111', true, '666554411', 1, 'maria@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Martin Soria', '12845678A', 'Cristina', '1111', true, '666554411', 1, 'cristina@correo.com');
+INSERT INTO usuaris (activo, apellidos, dni, nombre, password, privacidad_firmada, telefono, id_rol, email) VALUES(true, 'Díaz Martos', '12945679A', 'Dolores', '2222', true, '666554422', 2, 'lola@correo.com');
