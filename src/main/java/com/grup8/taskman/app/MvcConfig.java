6d31471b4ff382c974a50f8b1dc780a7ee1521b4
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-11-08 17:15:45 +0100

    Add files via upload

--- src/main/java/com/grup8/taskman/app/MvcConfig.java
+++ src/main/java/com/grup8/taskman/app/MvcConfig.java
@@ -1,14 +1,33 @@
 package com.grup8.taskman.app;
 
-import java.nio.file.Paths;
 
+import java.nio.file.Paths;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Classe de configuració que enregistra la carpeta uploads que farem servir
+ * per guardar les imatges. 
+ * 
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
 
+	/**
+	 * Funció que afegeix un recurs al registre de recursos de la aplicació.
+	 * En aquest cas afegeix uploads que és una carpeta que agafarà el path absolut
+	 * del projecte i s'afegirà a l'alçada de src. Aixó evita que hagi de posar
+	 * una ubicació dona't que no conec el sistema operatiu on treballarà el programa.
+	 * 
+	 * Més endavant, si donès temps es crearà una èina de configuració dins de
+	 * l'aplicació perquè sigui el superusuari qui decideixi a quina carpeta vol
+	 * guardar les imatges.
+	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
