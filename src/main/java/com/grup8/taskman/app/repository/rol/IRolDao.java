6f6eb2d46f62caf53572ba77566e7526fb43956d
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-11-08 17:18:52 +0100

    Add files via upload

--- src/main/java/com/grup8/taskman/app/repository/rol/IRolDao.java
+++ src/main/java/com/grup8/taskman/app/repository/rol/IRolDao.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.grup8.taskman.app.domain.usuaris.Rol;
 
+/**
+ * Interficie que estén de CrudRepository i que per tant tindrà les funcionalitats que aquesta ofereix.
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
 public interface IRolDao extends CrudRepository<Rol, Long>{
 		
 		
