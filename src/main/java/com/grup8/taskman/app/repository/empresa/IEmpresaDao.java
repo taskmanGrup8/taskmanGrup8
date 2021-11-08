893566a11ad96286771bda588e6bb19a603acf01
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-11-08 17:18:24 +0100

    Add files via upload

--- src/main/java/com/grup8/taskman/app/repository/empresa/IEmpresaDao.java
+++ src/main/java/com/grup8/taskman/app/repository/empresa/IEmpresaDao.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.grup8.taskman.app.domain.empreses.Empresa;
 
+/**
+ * Interficie que estén de CrudRepository i que per tant tindrà les funcionalitats que ofereix la interfície
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
 public interface IEmpresaDao extends CrudRepository<Empresa, Integer> {
 
 }
