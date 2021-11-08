5bbef110eff7fea2c1a5abec16fede377c166dc4
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-11-08 17:16:38 +0100

    Add files via upload

--- src/main/java/com/grup8/taskman/app/domain/departaments/Departament.java
+++ src/main/java/com/grup8/taskman/app/domain/departaments/Departament.java
@@ -1,9 +1,9 @@
 package com.grup8.taskman.app.domain.departaments;
 
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -14,11 +14,18 @@ import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-
 import com.grup8.taskman.app.domain.usuaris.Usuari;
 
-@Entity
-@Table(name="departaments")
+/**
+ * Classe que mapejem als camps de la taula departaments i que anotarem amb les validacions que han de tenir als 
+ * formularis. Està implementada amb JPA i anotada amb javax.Validation
+ * @author Sergio Estebam Gutiérrez
+ * @version 1.0.0
+ *
+ */
+
+@Entity // Indiquem que volem mapejar la classe a la base de dades
+@Table(name="departaments") // Associem la classe amb la taula departaments de la base de dades
 public class Departament implements Serializable {
 	
 	/**
@@ -26,28 +33,41 @@ public class Departament implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-
+	// ATRIBUTS
+	
+	
+	// Attribut que s'autogenerarà i que marquem con id a la base de dades
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
 	private Long id;
 	
+	// Indiquem que codigo no pot ser null i que és únic a la base de dades. també indiquem que la validació no pot ser ni nul 
+	// ni espais en blanc.
 	@NotBlank
 	@Column(name="codigo", unique=true, length=3, nullable=false, updatable=true )
 	private String codigo;
 	
+	// Indiquem que nombre no pot ser null i que és únic a la base de dades. també indiquem que la validació no pot ser ni nul 
+	// ni espais en blanc.
 	@NotBlank
 	@Column(name="nombre", unique=true, length=20, nullable=false, updatable=true )
 	private String nombre;	
 	
+	// Marquen la llista de usuaris com una relació many to many entre departaments i usuaris perquè un departament
+	// pot tenir més d'un usuari i aquest pot tenir més d'un departament. A més donem fetch lazy perquè poden haver molts
+	// usuaris i no cal descarregar-ho tot de cop.
 	@ManyToMany(mappedBy="departaments", fetch=FetchType.LAZY)
 	private List<Usuari> usuaris;
-		
+	
+	// CONSTRUCTOR
 	
 	public Departament() {		
 		
 		usuaris=new ArrayList<>();
 	}
+	
+	// GETTERS I SETTERS
 	public Long getId() {
 		return id;
 	}
@@ -74,40 +94,72 @@ public class Departament implements Serializable {
 		this.usuaris = usuaris;
 	}
 	
+	//MÈTODES
+	
+	/**
+	 * Mètode que afegeix un usuari a la llista d'usuaris
+	 * @param usuari Usuari que afegim a la llista
+	 */
 	public void addUsuari(Usuari usuari) {
 		
 		usuaris.add(usuari);
 	}
 	
+	/**
+	 * Mètode que elimina un usuari de la llista d'usuaris
+	 * @param usuari Usuari que volem eliminar de la llista
+	 */
 	public void removeUsuari(Usuari usuari) {
 		
 		usuaris.remove(usuari);
 	}	
 	
+	/**
+	 * Marquem aquest mètode amb @prePersist perquè volem que abans de guardar a la
+	 * base de dades passi codigo a majúscules.
+	 */
 	@PrePersist
 	public void prePersist() {
 		
 		this.codigo=codigo.toUpperCase();
 	}
 	
-	
+	/**
+	 * Fem override del mètode equals perque volem que les comparacions siguin personalitzades pel nom del departament
+	 * que és únic a la base de dades.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		
+		// Si obj és el mateix objecte que this retornem true
 		if(this==obj)return true;
+		// si obj és null retornem false
 		if(obj==null)return false;
+		
+		// Si obj no es de la mateixa classe Departament retornem false
 		if(getClass() !=obj.getClass())return false;
+		
+		// Fem cast d'obj
 		Departament departament=(Departament) obj;
+		
+		// Si l'atribut nombre és null i l'atribut nombre d'obj no llavors retornem false
 		if(nombre==null) {
 			
 			if(departament.getNombre()!=null)return false;
+			
+		// En cas contrari si nombre no és igual a l'atribut nombre d'obj retornem false 	
 		}else if(!nombre.equals(departament.getNombre())) {
 			
 			return false;
 		}
 		
+		// Retornem true perquè els dos noms son iguals
 		return true;
 	}
+	
+	/**
+	 * Fem override del métode hashCode perquè sigui personalitzat al camp nombre
+	 */
 	@Override
 	public int hashCode() {
 		final int prime=31;
