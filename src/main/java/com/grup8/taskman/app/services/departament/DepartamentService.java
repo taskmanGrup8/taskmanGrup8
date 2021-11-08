34d56f88c9adaa769111de6026afcf3c6eb9bca1
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-11-08 17:19:40 +0100

    Add files via upload

--- src/main/java/com/grup8/taskman/app/services/departament/DepartamentService.java
+++ src/main/java/com/grup8/taskman/app/services/departament/DepartamentService.java
@@ -9,20 +9,45 @@ import org.springframework.transaction.annotation.Transactional;
 import com.grup8.taskman.app.domain.departaments.Departament;
 import com.grup8.taskman.app.repository.departament.IDepartamentDao;
 
+/**
+ * Classe que implementa la interficie IDepartamentService i que retorna els resultats del DAO.
+ * Està marcada com Service per poder ser injectada posteriorment. Els diferents mètodes estan
+ * anotats amb @Transactional per indicar que fan accions sobre la base de dades. Les que tenen 
+ * l'atribut readOnly=true indiquen que nomès són de consulta.
+ * 
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
+
 @Service
 public class DepartamentService implements IDepartamentService {
 
+	// Injectem la interfície del repository
 	@Autowired
 	IDepartamentDao seccioDao;
 
+	/**
+	 * Mètode que guarda/actualitza els elements a la base de dades.
+	 * @return Retorna l'element guardat o null si no ho ha pogut realitzar.
+	 */
 	@Override
 	@Transactional
-	public void save(Departament departament) {
+	public Departament save(Departament departament) {
 		
-		seccioDao.save(departament);
+		Departament result=seccioDao.save(departament);
+		// El mètode save mai retorna null, en canvi si no ho ha pogut guarda l'id que retorna si que és null
+		// Si l'id és null llavors retornem null
+		if(result.getId()==null)return null;
+		// Retornem el departament guardat.
+		return result;
 
 	}
 
+	/**
+	 * Mètode que busca la llista completa d'elements de la taula departaments
+	 * @return Retorna la llista trobada
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Departament> findAll() {
@@ -30,12 +55,21 @@ public class DepartamentService implements IDepartamentService {
 		return (List<Departament>) seccioDao.findAll();
 	}
 
+	/**
+	 * Mètode que busca el departament amb l'id indicat
+	 * @return Retorna el departament trobat o null si no ha trobat cap
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public Departament findById(Long id) {
 		return seccioDao.findById(id).orElse(null);
 	}
 
+	
+	/**
+	 * Mètode que busca el departament amb el codi indicat
+	 * @return Retorna el departament trobat o null si no ha trobat cap
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public Departament findByCodi(String codi) {
@@ -43,6 +77,10 @@ public class DepartamentService implements IDepartamentService {
 		return seccioDao.findByCodigo(codi).orElse(null);
 	}
 
+	/**
+	 * Mètode que busca el departament amb el nom indicat
+	 * @return Retorna el departament trobat o null si no ha trobat cap
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public Departament findByNom(String nom) {
@@ -50,6 +88,9 @@ public class DepartamentService implements IDepartamentService {
 	}
 	
 
+	/**
+	 * Mètode que elimina el departament passat per paràmetre
+	 */
 	@Override
 	@Transactional
 	public void delete(Departament departament) {
@@ -58,6 +99,9 @@ public class DepartamentService implements IDepartamentService {
 
 	}
 
+	/**
+	 * Mètode que elimina tots els departaments de la base de dades
+	 */
 	@Override
 	@Transactional
 	public void deleteAll() {
@@ -66,6 +110,10 @@ public class DepartamentService implements IDepartamentService {
 		
 	}
 
+	/**
+	 * Mètode que busca una llista dels departaments que el seu camp codi comenci pel paràmetre passat.
+	 * @return Retorna la llista trobada
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Departament> findByCodigoStartsWith(String codigo) {
@@ -73,6 +121,11 @@ public class DepartamentService implements IDepartamentService {
 		return seccioDao.findByCodigoStartsWith(codigo);
 	}
 
+	
+	/**
+	 * Mètode que busca una llista dels departaments que el seu camp nom comenci pel paràmetre passat.
+	 * @return Retorna la llista trobada
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Departament> findByNombreStartsWith(String nombre) {
--- src/main/java/com/grup8/taskman/app/services/departament/IDepartamentService.java
+++ src/main/java/com/grup8/taskman/app/services/departament/IDepartamentService.java
@@ -1,20 +1,26 @@
 package com.grup8.taskman.app.services.departament;
 
 import java.util.List;
-
 import com.grup8.taskman.app.domain.departaments.Departament;
+
+/**
+ * Interfície que determina les funcionalitats que tindrà la classe de servei que implementa el DAO de departaments. 
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
 
 public interface IDepartamentService {
 	
-	public void save(Departament departament);
-	public List<Departament> findAll();
-	public Departament findById(Long id);
-	public Departament findByCodi(String codi);
-	public Departament findByNom(String nom);	
-	public void delete(Departament departament);
-	public void deleteAll();
-	public List<Departament> findByCodigoStartsWith(String codigo);
-	public List<Departament> findByNombreStartsWith(String nombre);
+	public Departament save(Departament departament); // Mètode per guardar departaments
+	public List<Departament> findAll(); // Mètode per buscar tots els departaments
+	public Departament findById(Long id); // Mètode per buscar un departament per Id
+	public Departament findByCodi(String codi); // Mètode per buscar un departament pel camp codigo
+	public Departament findByNom(String nom); // Mètode per buscar un departament pel camp codigo	
+	public void delete(Departament departament); // Mètode per eliminar un departament
+	public void deleteAll(); // Mètode per eliminar tots els departaments
+	public List<Departament> findByCodigoStartsWith(String codigo); // Mètode per buscar la llista d'usuaris que tingui el camp codigo que comenci pel paràmetre indicat
+	public List<Departament> findByNombreStartsWith(String nombre); // Mètode per buscar la llista d'usuaris que tingui el camp nombre que comenci pel paràmetre indicat
 	
 
 }
