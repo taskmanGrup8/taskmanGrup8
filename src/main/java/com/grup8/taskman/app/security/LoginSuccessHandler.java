329fe5969988df2a778b2d179e6bbe25815d78f4
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-11-19 13:15:47 +0100

    Add files via upload

---
+++ src/main/java/com/grup8/taskman/app/security/LoginSuccessHandler.java
@@ -0,0 +1,31 @@
+package com.grup8.taskman.app.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.support.SessionFlashMapManager;
+
+@Component
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+		SessionFlashMapManager flashMapManager=new SessionFlashMapManager();		
+		FlashMap flashMap=new FlashMap();
+		flashMap.put("success", "Benvingut " + authentication.getName());
+		flashMapManager.saveOutputFlashMap(flashMap, request, response);		
+		super.onAuthenticationSuccess(request, response, authentication);
+	}
+	
+	
+
+}
