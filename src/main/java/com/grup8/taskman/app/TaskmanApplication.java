170eceae41d730696408c47271d1e728e410d329
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2021-12-27 17:19:33 +0100

    mail asincrono

--- src/main/java/com/grup8/taskman/app/TaskmanApplication.java
+++ src/main/java/com/grup8/taskman/app/TaskmanApplication.java
@@ -2,8 +2,10 @@ package com.grup8.taskman.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TaskmanApplication {
 
 	public static void main(String[] args) {
