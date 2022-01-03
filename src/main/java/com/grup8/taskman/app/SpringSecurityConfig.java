01749225631fce798873a4e99b7461c4fda26ca4
rortegame <raquelortegamercader@gmail.com>
2022-01-03 20:57:07 +0100

    Arreglat bug del footer + recuperació de la clau

--- src/main/java/com/grup8/taskman/app/SpringSecurityConfig.java
+++ src/main/java/com/grup8/taskman/app/SpringSecurityConfig.java
@@ -53,7 +53,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/img/**", "/plugins/**").permitAll()	
+		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/img/**", "/plugins/**", "/index", "/recover/**").permitAll()
 		.antMatchers("/uploads/**").hasAnyRole("USER")
 		.anyRequest().authenticated()
 		.and()
