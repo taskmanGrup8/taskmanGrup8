813c4bc1778d8162e300b3af6983a39cb9ff0539
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2022-01-07 20:18:28 +0100

    Add files via upload

--- src/main/java/com/grup8/taskman/app/SpringSecurityConfig.java
+++ src/main/java/com/grup8/taskman/app/SpringSecurityConfig.java
@@ -53,7 +53,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/img/**", "/plugins/**", "/recover/**").permitAll()	
+		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/img/**", "/plugins/**", "/recover/**", "/empreses/leerPrivacidad/**").permitAll()	
 		.antMatchers("/uploads/**").hasAnyRole("USER")
 		.anyRequest().authenticated()
 		.and()
