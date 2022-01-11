626c4ca6292bb80b86bee4f3a2fccfe7ea9f039e
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2022-01-11 23:08:53 +0100

    Entrega final

--- src/main/java/com/grup8/taskman/app/services/tasques/INotificacionService.java
+++ src/main/java/com/grup8/taskman/app/services/tasques/INotificacionService.java
@@ -5,14 +5,21 @@ import java.util.List;
 
 import com.grup8.taskman.app.domain.tasques.Notificacion;
 
+/**
+ * Interfície que determina les funcionalitats que tindrà la classe de servei que implementa el DAO de notificacions.
+ *  
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
 public interface INotificacionService {
 
 	
-	public Notificacion save(Notificacion notificacion);
-	public Notificacion findById(long id);
-	public List<Notificacion> findAll();
-	public void delete(Notificacion notificacion);
-	public List<Notificacion> findByFase(Long id);	
-	public List<Notificacion> findByUsuari(Long id);	
-	public List<Notificacion> findByUsuariEntreFechas(Long id, Date data1, Date data2);
+	public Notificacion save(Notificacion notificacion); // Mètode que guarda la notificació rebuda per paràmetre a la base de dades
+	public Notificacion findById(long id); // Mètode que cerca a la base de dades la notificació amb l'id indicat
+	public List<Notificacion> findAll(); // Mètode que cerca a la base de dades totes les notificacions
+	public void delete(Notificacion notificacion); // Mètode que elimina la notificació rebuda per paràmetre de la base de dades
+	public List<Notificacion> findByFase(Long id);	// Mètode que cerca la notificació que te associada una fase executable amb l'id rebut
+	public List<Notificacion> findByUsuari(Long id);// Mètode que cerca la notificació que te associat un usuari amb l'id rebut.	
+	public List<Notificacion> findByUsuariEntreFechas(Long id, Date data1, Date data2); // Mètode que cerca les notificacions compresses entre les dates indicades que pertayen a l'usuari rebut.
 }
--- src/main/java/com/grup8/taskman/app/services/tasques/NotificacionService.java
+++ src/main/java/com/grup8/taskman/app/services/tasques/NotificacionService.java
@@ -10,12 +10,28 @@ import org.springframework.transaction.annotation.Transactional;
 import com.grup8.taskman.app.domain.tasques.Notificacion;
 import com.grup8.taskman.app.repository.tasques.INotificacionDao;
 
+/**
+ * Classe que implementa la interficie INotificacionService i que retorna els resultats del DAO.
+ * Està marcada com Service per poder ser injectada posteriorment. Els diferents mètodes estan
+ * anotats amb @Transactional per indicar que fan accions sobre la base de dades. Les que tenen 
+ * l'atribut readOnly=true indiquen que nomès són de consulta.
+ * 
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
 @Service
 public class NotificacionService implements INotificacionService{
 
+	// Injectem la interfície del repository
 	@Autowired
 	INotificacionDao notificacionDao;
 	
+	/**
+	 * Mètode que guarda la notificació rebuda per paràmetre a la base de dades
+	 * @param notificacion Notificació que volem guardar
+	 * @return Retorna la notificació guardada
+	 */
 	@Override
 	@Transactional
 	public Notificacion save(Notificacion notificacion) {
@@ -23,6 +39,11 @@ public class NotificacionService implements INotificacionService{
 		return notificacionDao.save(notificacion);
 	}
 
+	/**
+	 * Mètode que cerca a la base de dades la notificació amb l'id indicat
+	 * @param id Id de la notificació que volem cercar
+	 * @return Retorna la notificació trobada o null en cas de no trobar cap
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public Notificacion findById(long id) {
@@ -45,6 +66,11 @@ public class NotificacionService implements INotificacionService{
 		
 	}
 
+	/**
+	 * Mètode que cerca la notificació que te associada una fase executable amb l'id rebut
+	 * @param id Id de la fase executable que volem que estigui associada a la notificació
+	 * @return Retorna les notificacions trobades
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Notificacion> findByFase(Long id) {
@@ -52,6 +78,11 @@ public class NotificacionService implements INotificacionService{
 		return notificacionDao.findByFase(id);
 	}
 
+	/**
+	 * Mètode que cerca la notificació que te associat un usuari amb l'id rebut.	
+	 * @param id Id de l'usuari que volem que estigui associat a la notificació
+	 * @return Retorna les notificacions trobades
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Notificacion> findByUsuari(Long id) {
@@ -59,6 +90,13 @@ public class NotificacionService implements INotificacionService{
 		return notificacionDao.findByUsuari(id);
 	}
 
+	/**
+	 * Mètode que cerca les notificacions compresses entre les dates indicades que pertayen a l'usuari rebut.
+	 * @param id Identificador de l'usuari a qui volem que pertanyn les notificacions
+	 * @param data1 Data d'inici del rang de dates
+	 * @param data2 Data de fi del rang de dates
+	 * @return Retorna les notificacions trobades
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Notificacion> findByUsuariEntreFechas(Long id, Date data1, Date data2) {
