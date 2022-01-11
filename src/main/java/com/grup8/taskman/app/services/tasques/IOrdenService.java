3f266734f3d5d17addef3d41e000a993e3557af5
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2022-01-11 23:29:33 +0100

    Add files via upload

--- src/main/java/com/grup8/taskman/app/services/tasques/IOrdenService.java
+++ src/main/java/com/grup8/taskman/app/services/tasques/IOrdenService.java
@@ -5,13 +5,19 @@ import java.util.List;
 
 import com.grup8.taskman.app.domain.tasques.Orden;
 
+/**
+* Interfície que determina les funcionalitats que tindrà la classe de servei que implementa el DAO de d'ordres. 
+* @author Sergio Esteban Gutiérrez
+* @version 1.0.0
+*
+*/
 public interface IOrdenService {
 
-	public Orden save(Orden orden);
-	public Orden findById(Long id);
-	public List<Orden> findAll();
-	public List<Orden> findByNotificada(boolean notificada);
-	public List<Orden> findByEntreDates(Date data1, Date data2);
-	public void delete(Orden orden);
-	public List<Orden> findByNomTasca(String nomTasca); 
+	public Orden save(Orden orden); // Mètode que guarda/actualitza els elements a la base de dades.
+	public Orden findById(Long id); // Mètode que busca l'ordre amb l'id indicat
+	public List<Orden> findAll(); // Mètode que busca la llista completa d'elements de la taula ordres
+	public List<Orden> findByNotificada(boolean notificada); // Mètode que cerca la llista d'ordres que el seu camp notificada coincideix al el paràmetre passat
+	public List<Orden> findByEntreDates(Date data1, Date data2); // Mètode que cerca totes les ordres que el seu camp dataFin estigui comprés al rang de dates passades per paràmetre.
+	public void delete(Orden orden); // Mètode que elimina l'ordre passada per paràmetre
+	public List<Orden> findByNomTasca(String nomTasca); // Mètode que busca l'ordre que te una tasca amb el nom indicat
 }
--- src/main/java/com/grup8/taskman/app/services/tasques/OrdenService.java
+++ src/main/java/com/grup8/taskman/app/services/tasques/OrdenService.java
@@ -10,13 +10,28 @@ import org.springframework.transaction.annotation.Transactional;
 import com.grup8.taskman.app.domain.tasques.Orden;
 import com.grup8.taskman.app.repository.tasques.IOrdenDao;
 
+/**
+ * Classe que implementa la interficie IOrdenService i que retorna els resultats del DAO.
+ * Està marcada com Service per poder ser injectada posteriorment. Els diferents mètodes estan
+ * anotats amb @Transactional per indicar que fan accions sobre la base de dades. Les que tenen 
+ * l'atribut readOnly=true indiquen que nomès són de consulta.
+ * 
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
 @Service
 public class OrdenService implements IOrdenService{
 
-	@Autowired
-	
+	// Injectem la interfície del repository
+	@Autowired	
 	IOrdenDao ordenDao;
 	
+	/**
+	 * Mètode que guarda/actualitza els elements a la base de dades.
+	 * @param orden Ordre que volem guardar
+	 * @return Retorna l'element guardat o null si no ho ha pogut realitzar.
+	 */
 	@Override
 	@Transactional
 	public Orden save(Orden orden) {
@@ -24,6 +39,11 @@ public class OrdenService implements IOrdenService{
 		return ordenDao.save(orden);
 	}
 
+	/**
+	 * Mètode que busca l'ordre amb l'id indicat
+	 * @param id Id de l'ordre que volem cercar
+	 * @return Retorna l'ordre trobada o null si no ha trobat cap
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public Orden findById(Long id) {
@@ -31,6 +51,10 @@ public class OrdenService implements IOrdenService{
 		return ordenDao.findById(id).orElse(null);
 	}
 
+	/**
+	 * Mètode que busca la llista completa d'elements de la taula ordres
+	 * @return Retorna la llista trobada
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Orden> findAll() {
@@ -38,6 +62,11 @@ public class OrdenService implements IOrdenService{
 		return (List<Orden>) ordenDao.findAll();
 	}
 
+	/**
+	 * Mètode que cerca la llista d'ordres que el seu camp notificada coincideix al el paràmetre passat
+	 * @param notificada Estat del camp notificada de l'ordre.
+	 * @return Retorna la llista trobada
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Orden> findByNotificada(boolean notificada) {
@@ -45,6 +74,12 @@ public class OrdenService implements IOrdenService{
 		return ordenDao.findByNotificada(notificada);
 	}
 
+	/**
+	 * Mètode que cerca totes les ordres que el seu camp dataFin estigui comprés al rang de dates passades per paràmetre. 
+	 * @param data1 Data inici del rang
+	 * @param data2 Data fi del rang
+	 * @return Retorna el llistat d'ordres trobat
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Orden> findByEntreDates(Date data1, Date data2) {
@@ -52,6 +87,10 @@ public class OrdenService implements IOrdenService{
 		return ordenDao.findAllByDataFinBetween(data1, data2);
 	}
 
+	/**
+	 * Mètode que elimina l'ordre passada per paràmetre
+	 * @param orden Ordre que volem eliminar
+	 */
 	@Override
 	@Transactional
 	public void delete(Orden orden) {
@@ -60,13 +99,16 @@ public class OrdenService implements IOrdenService{
 		
 	}
 
+	/**
+	 * Mètode que busca l'ordre que te una tasca amb el nom indicat
+	 * @param nomTasca Nom que ha de tenir la tasca associada a l'ordre
+	 * @return Retorna la llista trobada.
+	 */
 	@Override
 	@Transactional(readOnly=true)
 	public List<Orden> findByNomTasca(String nomTasca) {
 
 		return ordenDao.findByNomTasca(nomTasca);
-	}
-	
-	
+	}	
 
 }
