c3b1003abf4f0c5a35d26763dd25ad1a6ad2ff5e
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2022-01-11 21:13:29 +0100

    Entrega final

--- src/main/java/com/grup8/taskman/app/repository/passwordRecover/IPasswordRecoverDao.java
+++ src/main/java/com/grup8/taskman/app/repository/passwordRecover/IPasswordRecoverDao.java
@@ -5,9 +5,20 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import com.grup8.taskman.app.domain.usuaris.PasswordRecover;
-
+/**
+ * Interfície que estén de CrudRepository i que per tant tindrà les funcionalitats que ofereix la interfície
+ * 
+ * @author Sergio Esteban Gutiérrez
+ * @version 1.0.0
+ *
+ */
 public interface IPasswordRecoverDao extends CrudRepository<PasswordRecover, Long>{
 	
+	/**
+	 * Funció que busca a la base de dades la llista de PasswordRecover que el seu camp link sigui igual al passat per paràmetre
+	 * @param link Link que volem cercar
+	 * @return Retorna un Optional amb la llista trobada.
+	 */
 	public Optional<PasswordRecover> findByLink(String link);
 
 }
