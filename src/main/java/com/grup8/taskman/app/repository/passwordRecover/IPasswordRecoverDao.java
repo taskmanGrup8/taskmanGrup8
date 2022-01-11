772938fabb937cc789921f7feb6a0f915731d231
taskmanGrup8 <92634987+taskmanGrup8@users.noreply.github.com>
2022-01-11 21:21:17 +0100

    Entrega final

--- src/main/java/com/grup8/taskman/app/repository/passwordRecover/IPasswordRecoverDao.java
+++ src/main/java/com/grup8/taskman/app/repository/passwordRecover/IPasswordRecoverDao.java
@@ -15,9 +15,9 @@ import com.grup8.taskman.app.domain.usuaris.PasswordRecover;
 public interface IPasswordRecoverDao extends CrudRepository<PasswordRecover, Long>{
 	
 	/**
-	 * Funció que busca a la base de dades la llista de PasswordRecover que el seu camp link sigui igual al passat per paràmetre
+	 * Funció que busca a la base de dades el PasswordRecover que el seu camp link sigui igual al passat per paràmetre
 	 * @param link Link que volem cercar
-	 * @return Retorna un Optional amb la llista trobada.
+	 * @return Retorna un Optional amb l'element trobat.
 	 */
 	public Optional<PasswordRecover> findByLink(String link);
 
